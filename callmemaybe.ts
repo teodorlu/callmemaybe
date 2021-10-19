b1880eca1562b924c9cc2f2de08db59541606236
Teodor Heggelund <teodor+git@iterate.no>
2021-10-19 19:16:21 +0200

    Do some literate stuff

--- callmemaybe.ts
+++ callmemaybe.ts
@@ -1,7 +1,16 @@
+import * as t from "https://deno.land/std@0.112.0/testing/asserts.ts";
+
+// sette korrekt LSP i Emacs: M-x +lsp/switch-client RET deno-ls RET RET
+// sette korrekt LSP i VSCode: TODO (Oddmund hjelp?)
+
 interface Booyah {
     fwiw: string;
 }
 
-console.log("wtf")
-console.log("wtf")
-console.log("wtf")
+console.log("Printing out stuff!!!")
+
+// Vi prøver oss på å skrive testdrevet og "literate". Det betyr at du skal
+// kunne lese denne filen som en bok, og vi kontrollerer underveis at ting
+// funker med asserts.
+
+t.assertEquals(1, 1);
